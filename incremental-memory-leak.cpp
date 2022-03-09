f2b1cadb61ee1f10f3c0e262a6c2d24e3dbffb28
Herakles100 <48856045+Herakles100@users.noreply.github.com>
2022-03-09 14:06:49 -0500

    Create incremental-memory-leak.cpp

---
+++ incremental-memory-leak.cpp
@@ -0,0 +1,28 @@
+// Problem statement: https://leetcode.com/problems/incremental-memory-leak/
+// Author: https://github.com/Herakles100
+// Time complexity: O(sqrt(n1+n2))
+// Space complexity: O(1)
+// Brute force approach
+
+class Solution {
+public:
+    vector<int> memLeak(int memory1, int memory2) {
+        int i = 1;
+        while(max(memory1,memory2) >= i){
+            if(memory1 >= memory2){
+                memory1 -= i;
+            }
+            else memory2 -= i;
+            ++i;
+        }
+        // if(memory1>0 || memory2>0){
+        //     if(memory1 >= memory2){
+        //             memory1 -= i;
+        //         }
+        //     else memory2 -= i;
+        //     ++i;
+        // }
+        // if(memory1<=0 && memory2<=0)
+        return {i,memory1,memory2};
+    }
+};
